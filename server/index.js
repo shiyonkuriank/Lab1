266a8d70944beea90f06e3086faaf7f32df44709
shiyonkuriank <shiyonkuriansjsu@gmail.com>
2021-10-10 16:34:19 -0700

    modified ip

--- server/index.js
+++ server/index.js
@@ -1088,6 +1088,6 @@ app.post("/uploadUserImage", upload.single('image'), (req,res)=>{
   res.send("Image Uploaded")
 })
 
-app.listen('18.218.134.220:3001', ()=>{
+app.listen('http://18.218.134.220:3001', ()=>{
 console.log("Server running on 3001");
 });
